ce4e1429af14f68a6a84102db208e524e1e0b282
altazur <altazur.91@gmail.com>
2019-12-11 00:01:46 +0200

    Click issue fixed

--- blog/blogapp/static/blogapp/js/postlike.js
+++ blog/blogapp/static/blogapp/js/postlike.js
@@ -1,8 +1,10 @@
-$('#like').click(function(){
-	var post_id;
-	got_post_id = $(this).attr("data-postid");
-	$.get('/likepost/', {post_id: got_post_id}, function(data){
-		$('#likes').html(data);
-		$('#like').hide();
+$(function(){
+	$('#like').click(function(){
+		var post_id;
+		got_post_id = $(this).attr("data-postid");
+		$.get('/likepost/', {post_id: got_post_id}, function(data){
+			$('#likes').html(data);
+			$('#like').hide();
+		});
 	});
 });
