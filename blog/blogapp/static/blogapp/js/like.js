0217bee683b3c80cce0da51f4813085396dd5a21
altazur <altazur.91@gmail.com>
2019-12-11 22:16:58 +0200

    Fix bugs

--- blog/blogapp/static/blogapp/js/like.js
+++ blog/blogapp/static/blogapp/js/like.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
 	$('.like').click(function(){
 		var element_id;
+		var element_type;
+		element_type = $(this).parents('.post').length ? 'post' : 'comment';
 		element_id = $(this).attr("data-id");
-		$.get('/likepost/', {post_id: element_id}, function(data){
-			$('#likes'+element_id).html(data);
-			$('#like'+element_id).hide();
+		//I don't proud if this if else statement
+		if (element_type == 'post'){
+			$.get('/likepost/', {post_id: element_id}, function(data){
+				$('#likes'+element_id).html(data);
+				$('#like'+element_id).hide();
 		});
+		}
+		else if (element_type == 'comment'){
+			$.get('/likecomment/', {comment_id: element_id}, function(data){
+				$('#likes'+element_id).html(data);
+				$('#like'+element_id).hide();
+		});
+
+		}
 	});
 });
