966aeb4300faf77f9caf03527e7848dbd110c508
altazur <altazur.91@gmail.com>
2019-12-11 21:54:04 +0200

    Rename script and made like.js more reusable

--- blog/blogapp/static/blogapp/js/like.js
+++ blog/blogapp/static/blogapp/js/like.js
@@ -1,1 +1,10 @@
-//Code will be here
+$(document).ready(function(){
+	$('.like').click(function(){
+		var element_id;
+		element_id = $(this).attr("data-id");
+		$.get('/likepost/', {post_id: element_id}, function(data){
+			$('#likes'+element_id).html(data);
+			$('#like'+element_id).hide();
+		});
+	});
+});
