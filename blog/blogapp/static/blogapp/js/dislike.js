d2b99c3913c8176d77408f72479abbf023f7ddaf
altazur <altazur.91@gmail.com>
2019-12-11 22:22:38 +0200

    Init commit

---
+++ blog/blogapp/static/blogapp/js/dislike.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$('.dislike').click(function(){
+		var element_id;
+		var element_type;
+		element_type = $(this).parents('.post').length ? 'post' : 'comment';
+		element_id = $(this).attr("data-id");
+		//I don't proud of this if else statement :)
+		if (element_type == 'post'){
+			$.get('/dislikepost/', {post_id: element_id}, function(data){
+				$('#dislikes'+element_id).html(data);
+				$('#dislike'+element_id).hide();
+		});
+		}
+		else if (element_type == 'comment'){
+			$.get('/dislikecomment/', {comment_id: element_id}, function(data){
+				$('#dislikes'+element_id).html(data);
+				$('#dislike'+element_id).hide();
+		});
+
+		}
+	});
+});
