02cd5d410a67f49ae3e403b3b2eca348dc0fc834
altazur <altazur.91@gmail.com>
2020-01-15 22:52:55 +0200

    Remove utils functions to another module

--- blog/blogapp/tests/tests_unit.py
+++ blog/blogapp/tests/tests_unit.py
@@ -4,29 +4,10 @@ from blogapp.models import Post, Comment
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
+from .utils import create_user, create_post, create_comment
 
 # Create your tests here.
-# Utils methods
-def create_user(name="Test", password="test"):
-    """Create user with given name. 'Test' is the default one"""
-    user = User.objects.create(username=name)
-    user.set_password(password)
-    user.save()
-    return user
-
-def create_post(post_text, days, user, tags):
-    """Create post with given text, date and tags for user
-    Negative days stands for past from now
-    Positive dats is the future from now question"""
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Post.objects.create(text=post_text, user=user, tag=tags, pub_date=time)
-
-def create_comment(comment_text, post, days, user):
-    """Create comment with given text for given post and with given date by given user
-    Negative days stands for past from now comments
-    Positive days is future from now"""
-    time = timezone.now() + datetime.timedelta(days=days)
-    return post.comment_set.create(text=comment_text, post=post, pub_date=time, user=user)
+# Local utils methods
 
 def prepare_quick_post():
     """Quick post function"""
